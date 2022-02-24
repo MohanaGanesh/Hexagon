242cd40b03a2887c9fed41ac9dac144852bcd523
Ajaykumar M <m1ajay2001@gmail.com>
2022-02-24 09:20:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
-
+from flask import Flask,render_template
+from flask.templating import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask!'
+     return render_template('base.html')
 
 app.run(host='0.0.0.0', port=81)
