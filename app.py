7c343daa7845f2f83b89e147f9ae761bfca84270
Jeffrin Harris M <jeffrin.harris@gmail.com>
2022-02-24 10:09:49 +0530

    Restore1

--- app.py
+++ app.py
@@ -1,14 +1,3 @@
-<<<<<<< Updated upstream
-from flask import Flask,render_template
-from flask.templating import render_template
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-     return render_template('base.html')
-
-app.run(host='0.0.0.0', port=81)
-=======
 from flask import Flask, render_template
   
 app = Flask(__name__)
@@ -20,4 +9,4 @@ def homepage():
   
 if __name__ == '__main__':
     app.run()
->>>>>>> Stashed changes
+
